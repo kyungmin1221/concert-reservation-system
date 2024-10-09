b6f0f3c9bfbdb568789267d66a620af620cb9472
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-10-09 16:23:22 +0900

    initial commit

---
+++ src/main/java/com/example/concertreservationsystem/ConcertReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertReservationSystemApplication.class, args);
+    }
+
+}
