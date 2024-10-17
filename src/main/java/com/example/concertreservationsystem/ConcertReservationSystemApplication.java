3058eb8fb51e503fa1e46a610592ec5a31e6bae6
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-10-18 02:04:50 +0900

    feat : 콘서트 취소 스케쥴러 구현

--- src/main/java/com/example/concertreservationsystem/ConcertReservationSystemApplication.java
+++ src/main/java/com/example/concertreservationsystem/ConcertReservationSystemApplication.java
@@ -3,9 +3,11 @@ package com.example.concertreservationsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class ConcertReservationSystemApplication {
 
     public static void main(String[] args) {
