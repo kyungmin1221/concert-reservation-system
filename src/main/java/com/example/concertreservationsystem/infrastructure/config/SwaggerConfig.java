99e4b6a049a5c230744b82f9092af484d21de4e2
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-10-19 00:06:44 +0900

    feat : controller 에서 Swagger 설명 추가

--- src/main/java/com/example/concertreservationsystem/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/example/concertreservationsystem/infrastructure/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("항해 백엔드6기 콘서트 예약 시스템")
+                .title("항해 플러스 백엔드 6기 콘서트 예약 시스템 - 박경민")
                 .description("콘서트 예약 시스템에 관한 API")
                 .version("1.0.0");
     }
--- src/main/java/com/example/concertreservationsystem/web/controller/concert/ConcertController.java
+++ src/main/java/com/example/concertreservationsystem/web/controller/concert/ConcertController.java
@@ -3,13 +3,19 @@ package com.example.concertreservationsystem.web.controller.concert;
 import com.example.concertreservationsystem.domain.service.ConcertService;
 import com.example.concertreservationsystem.web.dto.concert.request.ConcertRequestDto;
 import com.example.concertreservationsystem.web.dto.concert.response.ConcertResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "콘서트 API", description = "콘서트 관련 API 입니다.")
+@Slf4j
 @RestController
 @RequestMapping("/api/concerts")
 @RequiredArgsConstructor
@@ -22,6 +28,14 @@ public class ConcertController {
      * @param requestDto
      * @return
      */
+    @Operation(
+            summary = "콘서트를 생성",
+            description = "콘서트를 생성합니다.",
+            parameters = {
+                    @Parameter(name = "name", description = "생성할 콘서트의 이름", example = "ive"),
+                    @Parameter(name = "price", description = "생성할 콘서트의 가격", example = "1000"),
+            }
+    )
     @PostMapping
     public ResponseEntity<ConcertResponseDto> registerConcert(@RequestBody ConcertRequestDto requestDto) {
         ConcertResponseDto responseDto = concertService.registerConcert(requestDto);
