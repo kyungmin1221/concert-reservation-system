246cc2662156748b764b6c745d591158597173a5
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-10-22 22:36:20 +0900

    feat : 콘서트 생성 단위 테스트 작성

---
+++ src/test/java/com/example/concertreservationsystem/concert/ConcertUnitTest.java
@@ -0,0 +1,53 @@
+package com.example.concertreservationsystem.concert;
+
+import com.example.concertreservationsystem.application.usecase.ConcertUseCase;
+import com.example.concertreservationsystem.domain.model.Concert;
+import com.example.concertreservationsystem.domain.repo.ConcertRepository;
+import com.example.concertreservationsystem.domain.service.ConcertService;
+import com.example.concertreservationsystem.infrastructure.persistence.JpaConcertRepository;
+import com.example.concertreservationsystem.web.dto.concert.request.ConcertRequestDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ConcertUnitTest {
+
+    @InjectMocks
+    private ConcertService concertService;
+
+    @Mock
+    private ConcertRepository concertRepository;
+
+    @DisplayName("콘서트 중복 생성 예외 테스트")
+    @Test
+    void 콘서트_중복_생성_예외() {
+
+        // given
+        String concertName = "ive";
+        Long concertPrice = 1000L;
+
+        ConcertRequestDto requestDto = new ConcertRequestDto();
+        requestDto.setName(concertName);
+        requestDto.setPrice(concertPrice);
+
+        when(concertRepository.findByName(concertName))
+                .thenReturn(Optional.of(new Concert()));
+
+        // when & then
+        assertThatThrownBy(() -> concertService.registerConcert(requestDto))
+                .isInstanceOf(IllegalArgumentException.class)  // 발생할 예외 타입 검증
+                .hasMessage("중복된 이름을 가진 콘서트는 등록할 수 없습니다.");  // 예외 메시지 검증
+    }
+
+}
