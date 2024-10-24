0978b83a66327bc8947d7b663c4314a67623f3cb
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-10-24 23:30:09 +0900

    feat : 예약 가능 날짜 및 좌석 조회 구현

--- src/main/java/com/example/concertreservationsystem/domain/repo/ConcertEventRepository.java
+++ src/main/java/com/example/concertreservationsystem/domain/repo/ConcertEventRepository.java
@@ -4,10 +4,15 @@ import com.example.concertreservationsystem.domain.model.Concert;
 import com.example.concertreservationsystem.domain.model.ConcertEvent;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ConcertEventRepository {
 
     //ConcertEvent save(ConcertEvent concertEvent);
     boolean existsByConcertAndEventDate(Concert concert, LocalDate eventDate);
 
+    // 가용 좌석이 있는 콘서트 이벤트 자동 쿼리 생성
+    List<ConcertEvent> findByAvailableSeatsGreaterThan(Long minSeats);
+
+    List<ConcertEvent> findAvailableConcertEvents();
 }
