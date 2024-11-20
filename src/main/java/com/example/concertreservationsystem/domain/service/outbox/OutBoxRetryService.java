0d9eeb84624e630b20b36db18f65d8656c43d5dd
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-11-21 01:07:46 +0900

    feat : outbox 를 통한 메시지 재발행 구현

---
+++ src/main/java/com/example/concertreservationsystem/domain/service/outbox/OutBoxRetryService.java
@@ -0,0 +1,41 @@
+package com.example.concertreservationsystem.domain.service.outbox;
+
+import com.example.concertreservationsystem.domain.constant.OutboxStatus;
+import com.example.concertreservationsystem.domain.model.PaymentOutBox;
+import com.example.concertreservationsystem.domain.repo.OutBoxRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OutBoxRetryService {
+
+    private final OutBoxRepository outboxRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Transactional
+    public void retryFailedMessages() {
+        List<PaymentOutBox> failedMessages = outboxRepository.findAllByStatus(OutboxStatus.FAILED);
+
+        for (PaymentOutBox outbox : failedMessages) {
+            try {
+                log.info("outbox 메시지 재발행 시작");
+                kafkaTemplate.send(outbox.getTopic(), outbox.getPayload()).get();
+
+                outbox.setStatusProcessed();
+                outboxRepository.save(outbox);
+
+                log.info("outbox 메시지 재발행 성공: {}", outbox.getId());
+
+            } catch (Exception e) {
+                log.error("outbox 메시지 재발행 실패: {}", outbox.getId(), e);
+            }
+        }
+    }
+}
